c57868a4dfdb02e63b97b98606c34cf37b2bab23
shinja <shinja.dev@gmail.com>
2017-04-28 22:40:34 +0800

    Rollback.

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,15 @@
 import Vue from 'vue';
-
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
-// import VuexPromiseMiddleware from 'vuex-promise-middleware';
+import VuexPromiseMiddleware from 'vuex-promise-middleware';
 import state from './state';
 import mutations from './mutations';
 import * as getters from './getters';
 import * as actions from './actions';
 
-function vuexPromiseMiddleware(store) {
-  store.subscribe((mutation) => {
-    const isPromise = mutation.payload && mutation.payload.then;
-
-    if (!isPromise) {
-      return;
-    }
-
-    const succeded = `${mutation.type}_SUCCEEDED`;
-    const failed = `${mutation.type}_FAILED`;
-
-    mutation.payload.then((response) => {
-      store.commit(succeded, response);
-    }).catch((response) => {
-      store.commit(failed, response);
-    });
-  });
-}
-
 Vue.use(Vuex);
-const plugins = [createLogger(), vuexPromiseMiddleware];
+
+const plugins = [createLogger(), VuexPromiseMiddleware];
 
 export default new Vuex.Store({
   state,
